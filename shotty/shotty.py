1975f0194c3fc3f301d66b0f3df06eb6573a1454
Aparna Mane <aparna@Aparnas-MacBook-Pro.local>
2019-09-11 15:49:54 +1000

    Updated the python script

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,25 @@
 import boto3
+import click
+# import sys
+
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instnaces():
+    "List EC2 instnaces"
+    for i in ec2.instances.all():
+        print(','.json((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+    return
+
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    # print(sys.argv)
+    list_instnaces()
